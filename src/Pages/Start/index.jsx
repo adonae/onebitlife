ba0c479c2771a68dde21e41f83c93e7559528e34
Adonae Vitorino <vitorino.adonae@gmail.com>
2022-11-28 22:13:28 -0300

    Inserindo Botão Padrão com os estilos e alguns alterações em outros arquivos

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import DefaultButton from "../../components/Common/DefaultButton";
+import LifeStatus from "../../components/Common/LifeStatus";
 
 export default function Start() {
+
+    const handleNavAppExplanation = () => {
+        console.log("Testando o clique");
+    };
+
     return (
         <View style={styles.container}>
             <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
@@ -10,9 +19,17 @@ export default function Start() {
                         source={require("../../assets/icons/logo3.png")}
                         style={styles.logo}
                     />
+                    <LifeStatus />
                     <Text style={styles.description}>
-                        Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
+                        Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"}{" "}
+                        o melhor nível.
                     </Text>
+                    <DefaultButton
+                        buttonText={"Continuar"}
+                        handlePress={handleNavAppExplanation}
+                        width={250}
+                        height={50}
+                    />
                 </View>
             </ScrollView>
         </View>
